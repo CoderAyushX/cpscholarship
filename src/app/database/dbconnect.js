4e873e8347f4ea427a308e9414b59d7f2c47ead1
Ayush kumar <ayushris49271@gmail.com>
2023-08-10 21:37:23 +0530

    form

--- src/app/database/dbconnect.js
+++ src/app/database/dbconnect.js
@@ -1,3 +1,4 @@
 const {uname, password} = process.env
-export const connectionStr = `mongodb+srv://${uname}:${password}@cluster0.yu1zz9r.mongodb.net/studentData?retryWrites=true&w=majority`
+export const connectionStr = `mongodb+srv://cpscholarship:cpscholarship@cluster0.yu1zz9r.mongodb.net/studentData?retryWrites=true&w=majority`
 
+// export const connectionStr = `mongodb+srv://${uname}:${password}@cluster0.yu1zz9r.mongodb.net/studentData?retryWrites=true&w=majority`
