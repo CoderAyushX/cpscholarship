2f7af3c84ada64201327b9fe6c207f80cd0c47ca
Ayush kumar <ayushris49271@gmail.com>
2023-08-14 19:31:49 +0530

    pdf maker

--- src/app/cpadminlink/form-table/page.js
+++ src/app/cpadminlink/form-table/page.js
@@ -1,13 +1,12 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
-import adminstyles from "@/app/style/admin.module.css";
+import adminstyles from "@/app/style/admin.module.css"
 import styles from "@/app/style/home.module.css";
 import Header from "@/app/components/Header";
 import axios from "axios";
 const Formdata = () => {
   const [data, setData] = useState([]);
-  const [isClicked, setIsClicked] = useState(false);
   const tableRef = useRef(null);
 
   useEffect(() => {
@@ -34,21 +33,21 @@ const Formdata = () => {
   function parseAndFormatDate(dateString) {
     // Regular expression to match the date format
     const dateFormatRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
-
-    if (typeof dateString === "string" && dateFormatRegex.test(dateString)) {
+  
+    if (typeof dateString === 'string' && dateFormatRegex.test(dateString)) {
       const dateObject = new Date(dateString);
-
+      
       const options = {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        timeZone: "Asia/Kolkata",
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZone: 'Asia/Kolkata'
       };
-
-      return dateObject.toLocaleDateString("en-IN", options);
+  
+      return dateObject.toLocaleDateString('en-IN', options);
     } else {
       return dateString;
     }
@@ -66,31 +65,28 @@ const Formdata = () => {
   };
 
   const csvGenerator = () => {
-    setIsClicked(true);
     const table = tableRef.current;
-    const rows = table.querySelectorAll("tbody tr");
+    const rows = table.querySelectorAll('tbody tr');
 
-    let csvContent = "";
+    let csvContent = '';
 
     // Adding headers
-    const headers = Array.from(table.querySelectorAll("thead th")).map(
-      (th) => th.textContent
-    );
-    csvContent += headers.join(",") + "\n";
+    const headers = Array.from(table.querySelectorAll('thead th')).map(th => th.textContent);
+    csvContent += headers.join(',') + '\n';
 
-    rows.forEach((row) => {
-      const cells = row.querySelectorAll("td");
-      const rowData = Array.from(cells).map((cell) => cell.textContent);
-      csvContent += rowData.join(",") + "\n";
+    rows.forEach(row => {
+        const cells = row.querySelectorAll('td');
+        const rowData = Array.from(cells).map(cell => cell.textContent);
+        csvContent += rowData.join(',') + '\n';
     });
 
     // Create a Blob object
-    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
 
     // Create a download link
-    const link = document.createElement("a");
-    link.setAttribute("href", URL.createObjectURL(blob));
-    link.setAttribute("download", "table-data.csv");
+    const link = document.createElement('a');
+    link.setAttribute('href', URL.createObjectURL(blob));
+    link.setAttribute('download', 'table-data.csv');
 
     // Append the link to the document
     document.body.appendChild(link);
@@ -100,26 +96,19 @@ const Formdata = () => {
 
     // Clean up
     document.body.removeChild(link);
-    setIsClicked(false);
-  };
+}
   return (
     <>
       <Header />
       <div className={adminstyles.textAndButton}>
-        <div className={adminstyles.howto_head_box}>
-          <h2>Student Form Data</h2>
-        </div>
-        {isClicked ? (
-          <button className={adminstyles.downloadButton}>
-            downloading....
-          </button>
-        ) : (
-          <button onClick={csvGenerator} className={adminstyles.downloadButton}>
-            download csv
-          </button>
-        )}
+      <div className={adminstyles.howto_head_box}>
+        <h2>Student Form Data</h2>
       </div>
-
+      <button onClick={csvGenerator} className={adminstyles.downloadButton}>
+          download csv
+        </button>
+      </div>
+      
       <div className={styles.question_break_box}>
         {data.length > 0 ? (
           <table className={styles.question_break} ref={tableRef}>
